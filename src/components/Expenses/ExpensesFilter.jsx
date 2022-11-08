198a723e5a5592574b124744f4baa6ba74eadadf
Rashford07 <adekunlerasheed333@gmail.com>
2022-11-08 22:41:53 +0100

    quick fix commit

--- src/components/Expenses/ExpensesFilter.jsx
+++ src/components/Expenses/ExpensesFilter.jsx
@@ -12,7 +12,7 @@ const ExpensesFilter = ({ onFilter }) => {
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select onClick={yearFilterHandler}>
+        <select onChange={yearFilterHandler}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
